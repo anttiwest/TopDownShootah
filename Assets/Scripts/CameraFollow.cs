55dad4402409c3361db85799d0cd0ad20dd7e0f6
antti laptop <antti_west@msn.com>
2018-01-26 15:25:36 +0000

    camera script, enemy movement script (not finished)

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject player;
+    Vector3 distance;
+    float smoothing = 10f;
+
+    private void Awake()
+    {
+        distance = transform.position - player.transform.position;
+    }
+
+    void FixedUpdate ()
+    {
+        LookAtPlayer();
+    }
+
+    void LookAtPlayer()
+    {
+        Vector3 targetPosition = player.transform.position + distance;
+        transform.position = Vector3.Lerp(targetPosition, player.transform.position, smoothing * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public GameObject player;
+    float speed = 2f;
+    Vector3 direction;
+
+    void Awake() {
+        Debug.Log("Enemy spawned");
+	}
+	
+	void FixedUpdate () {
+        transform.LookAt(player.transform);
+        MoveToPlayer();
+	}
+
+    void MoveToPlayer()
+    {
+        direction = player.transform.position;
+        transform.position += Time.deltaTime * speed * direction.normalized;
+        Debug.Log(direction);
+    }
+}
