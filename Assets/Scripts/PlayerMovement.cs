e8ba026b44bfbb3f2b95f11e175ceccfb9c531f6
antti laptop <antti_west@msn.com>
2018-01-26 12:49:57 +0000

    init

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public GameObject player;
+    public float speed;
+    Vector3 movementDirection;
+    float camRayLength = 100f;
+    int floorMask;
+
+	void Awake ()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+    }
+	
+	void FixedUpdate() {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Move(h, v);
+        Turn();
+    }
+
+    void Move(float h, float v)
+    {
+        movementDirection.Set(h, 0, v);
+        movementDirection = speed * Time.deltaTime * movementDirection.normalized;
+        transform.position += movementDirection;
+    }
+
+    void Turn()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit info;
+
+        if(Physics.Raycast(ray, out info))
+        {
+            Vector3 positionToLookAt = info.point;
+            positionToLookAt.y = 0f;
+            transform.LookAt(positionToLookAt);
+        }
+        Debug.DrawRay(ray.origin, 100 * ray.direction);
+    }
+}
