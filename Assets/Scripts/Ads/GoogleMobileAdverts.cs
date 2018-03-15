27174872be085a554b6d4101322d665174071a08
Antti West <antti_west@msn.com>
2018-03-15 10:27:18 +0000

    google banner ad

---
+++ Assets/Scripts/Ads/GoogleMobileAdverts.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using GoogleMobileAds.Api;
+using System;
+
+public class GoogleMobileAdverts : MonoBehaviour {
+
+    BannerView bannerView;
+
+    public void Start()
+    {
+        #if UNITY_ANDROID
+            string appId = "ca-app-pub-3940256099942544~3347511713";
+        #elif UNITY_IPHONE
+            string appId = "ca-app-pub-3940256099942544~1458002511";
+        #else
+            string appId = "unexpected_platform";
+        #endif
+
+        MobileAds.Initialize(appId);
+
+        RequestBanner();
+    }
+
+    private void RequestBanner()
+    {
+        #if UNITY_ANDROID
+            string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+        #elif UNITY_IPHONE
+            string adUnitId = "ca-app-pub-3940256099942544/2934735716";
+        #else
+            string adUnitId = "unexpected_platform";
+        #endif
+        
+        bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
+        AdRequest request = new AdRequest.Builder().Build();
+        bannerView.LoadAd(request);
+    }
+
+    public void HandleOnBannerAdLoaded(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLoaded event received");
+    }
+
+    public void HandleOnBannerAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        MonoBehaviour.print("HandleFailedToReceiveAd event received with message: "
+                            + args.Message);
+    }
+
+    public void HandleOnBannerAdOpened(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdOpened event received");
+    }
+
+    public void HandleOnBannerAdClosed(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdClosed event received");
+    }
+
+    public void HandleOnBannerAdLeftApplication(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLeftApplication event received");
+    }
+
+
+}
