b05a527781724cc3b4862b79320cb63376fb9d15
Antti West <antti_west@msn.com>
2018-03-16 09:19:09 +0000

    google rewarded video ad + play ad on restart

--- Assets/Scripts/Ads/GoogleMobileAdverts.cs
+++ Assets/Scripts/Ads/GoogleMobileAdverts.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
 using GoogleMobileAds.Api;
 using System;
+using UnityEngine.UI;
 
 public class GoogleMobileAdverts : MonoBehaviour {
 
     BannerView bannerView;
+    private RewardBasedVideoAd rewardBasedVideo;
+
+    Button playRewardVideoButton;
 
     public void Start()
     {
+        DontDestroyOnLoad(gameObject);
         #if UNITY_ANDROID
             string appId = "ca-app-pub-3940256099942544~3347511713";
         #elif UNITY_IPHONE
@@ -18,7 +23,15 @@ public class GoogleMobileAdverts : MonoBehaviour {
 
         MobileAds.Initialize(appId);
 
+        //Banner ads
         RequestBanner();
+
+        //Get singleton reward based video ad reference.
+        rewardBasedVideo = RewardBasedVideoAd.Instance;
+        RequestRewardedVideo();
+
+        playRewardVideoButton = GameObject.Find("PlayRewardVideoButton").GetComponent<Button>();
+        playRewardVideoButton.onClick.AddListener(PlayRewardedVideo);
     }
 
     private void RequestBanner()
@@ -31,11 +44,48 @@ public class GoogleMobileAdverts : MonoBehaviour {
             string adUnitId = "unexpected_platform";
         #endif
         
+        //bannerView.OnAdLoaded += HandleOnBannerAdLoaded;
+        //bannerView.OnAdFailedToLoad += HandleOnBannerAdFailedToLoad;
+        //bannerView.OnAdOpening += HandleOnBannerAdOpened;
+        //bannerView.OnAdClosed += HandleOnBannerAdClosed;
+        //bannerView.OnAdLeavingApplication += HandleOnBannerAdLeftApplication;
+
         bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
         AdRequest request = new AdRequest.Builder().Build();
         bannerView.LoadAd(request);
     }
 
+    void RequestRewardedVideo()
+    {
+        #if UNITY_ANDROID
+            string adUnitId = "ca-app-pub-3940256099942544/5224354917";
+        #elif UNITY_IPHONE
+            string adUnitId = "ca-app-pub-3940256099942544/1712485313";
+        #else
+            string adUnitId = "unexpected_platform";
+        #endif
+
+        rewardBasedVideo.OnAdLoaded += HandleRewardBasedVideoLoaded;
+        rewardBasedVideo.OnAdFailedToLoad += HandleRewardBasedVideoFailedToLoad;
+        rewardBasedVideo.OnAdOpening += HandleRewardBasedVideoOpened;
+        rewardBasedVideo.OnAdStarted += HandleRewardBasedVideoStarted;
+        rewardBasedVideo.OnAdRewarded += HandleRewardBasedVideoRewarded;
+        rewardBasedVideo.OnAdClosed += HandleRewardBasedVideoClosed;
+        rewardBasedVideo.OnAdLeavingApplication += HandleRewardBasedVideoLeftApplication;
+
+        AdRequest request = new AdRequest.Builder().Build();
+        rewardBasedVideo.LoadAd(request, adUnitId);
+    }
+
+    public void PlayRewardedVideo()
+    {
+        if (rewardBasedVideo.IsLoaded())
+        {
+            rewardBasedVideo.Show();
+        }
+    }
+
+    //BannerAd handlers
     public void HandleOnBannerAdLoaded(object sender, EventArgs args)
     {
         MonoBehaviour.print("HandleAdLoaded event received");
@@ -60,6 +110,48 @@ public class GoogleMobileAdverts : MonoBehaviour {
     public void HandleOnBannerAdLeftApplication(object sender, EventArgs args)
     {
         MonoBehaviour.print("HandleAdLeftApplication event received");
+    }
+
+    //RewardBasedVideo handlers
+    public void HandleRewardBasedVideoLoaded(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardBasedVideoLoaded event received");
+    }
+
+    public void HandleRewardBasedVideoFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        MonoBehaviour.print(
+            "HandleRewardBasedVideoFailedToLoad event received with message: "
+                             + args.Message);
+    }
+
+    public void HandleRewardBasedVideoOpened(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardBasedVideoOpened event received");
+    }
+
+    public void HandleRewardBasedVideoStarted(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardBasedVideoStarted event received");
+    }
+
+    public void HandleRewardBasedVideoClosed(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardBasedVideoClosed event received");
+    }
+
+    public void HandleRewardBasedVideoRewarded(object sender, Reward args)
+    {
+        string type = args.Type;
+        double amount = args.Amount;
+        MonoBehaviour.print(
+            "HandleRewardBasedVideoRewarded event received for "
+                        + amount.ToString() + " " + type);
+    }
+
+    public void HandleRewardBasedVideoLeftApplication(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleRewardBasedVideoLeftApplication event received");
     }
 
 
--- Assets/Scripts/Control/GameController.cs
+++ Assets/Scripts/Control/GameController.cs
@@ -4,23 +4,27 @@ using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour {
 
     public GameObject player;
+    GoogleMobileAdverts googleAds;
 
     void Awake()
     {
         player = GameObject.FindWithTag("Player");
         Cursor.visible = true;
+        googleAds = GameObject.Find("AdSystem").GetComponent<GoogleMobileAdverts>();
     }
 
     void FixedUpdate()
     {
         if (!GameObject.FindWithTag("Player") || Input.GetKeyDown(KeyCode.Escape))
         {
-            RestartGame();
+            LoadMenu();
         }
     }
 
-    public void RestartGame()
+    public void LoadMenu()
     {
+        googleAds.PlayRewardedVideo();
+
         //string scene = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene("Menu");
     }
